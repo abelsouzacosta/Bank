7c58b2a8ceb40e546a7b2668d682922d82cfd025
Junior <abelsouzacosta@gmail.com>
2023-09-03 17:33:53 -0300

    [chore]: adding class bank account

---
+++ src/bank/domain/entities/account/BankAccount.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bank.domain.entities.account;
+
+/**
+ *
+ * @author junior
+ */
+public class BankAccount {
+    private final String accountNumber;
+    
+    public BankAccount(String accountNumber) {
+        
+    }
+}
